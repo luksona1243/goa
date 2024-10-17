6d4880eb400de650f7e3eeb30e5d0584adc8c5ce
luksona1243 <luksona1243@gmail.com>
2024-10-17 22:46:22 +0400

    second commit

---
+++ 4/classwork/class4.py
@@ -0,0 +1,14 @@
+name = "luka"
+gardash = 12
+karen = 12.55
+print(name)
+print(gardash)
+print(karen)
+
+ricxvi = 10
+ricxvi2 = 20
+
+print(ricxvi + ricxvi2)
+print(ricxvi - ricxvi2)
+print(ricxvi / ricxvi2)
+print(ricxvi % ricxvi2)
---
+++ 4/homework.py/home4.py
@@ -0,0 +1,44 @@
+num1 = 5
+num2 = 10
+num3 = 15
+
+num = num1 + num2 + num3
+
+
+print("სამი რიცხვის ჯამი არის:", num)
+
+
+name = "ლუკა" 
+
+
+print("ჩემი სახელი არის:", name)
+
+
+height = 1.75  
+
+
+print("ჩემი სიმაღლე არის:", height)
+
+
+x = 15
+y =5  
+
+
+print("x ცვლადის მნიშვნელობა არის:", x + y)
+
+
+name = "ლუკა"  
+surname = "გიგოშვილი"  
+
+
+namee = name + " " + surname
+
+
+print("ჩემი სრული სახელი არის:", namee)
+
+
+price = 100
+price1 = 5  
+
+
+print("ფასი გამრავლებული 5-ზე არის:", price * 5)
