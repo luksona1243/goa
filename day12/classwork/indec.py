9ac182d2447225547470d6282d40064af57c4cad
luksona1243 <luksona1243@gmail.com>
2024-11-13 22:09:09 +0400

    second commit

---
+++ day12/classwork/indec.py
@@ -0,0 +1,5 @@
+num = 111
+num1 =1234
+print(num > num1 ,num == num1,num < num1, num >= num1, num <= num1)
+#buliani pasuxs abrunebs marto trus an fols 
+#shedarebis operatorebi gvwirdeba rom shevadarot ori ricxvi ertmanets an shemokvanili cifri an asaki an egeti ragac
