1a3c9764d9a1af69fa917de6ba4e4673f9c0845e
luksona1243 <luksona1243@gmail.com>
2025-01-12 21:22:03 +0400

    second commit

---
+++ day 24/classwork/cw.py
@@ -0,0 +1,11 @@
+name = print(input("shemoikvanet tqveni saxeli"))
+pas = print(input("shemoikvanet tqveni paroli"))
+
+name1 =""
+pas1 = ""
+while name != name1 or pas !=pas1:
+    print("arasworadaa paroli an saxeli")
+    name1 = print(input("shemoikvanet tqveni saxeli"))
+    pas1 = print(input("shemoikvanet tqveni paroli"))
+
+print("gamarjveba")
---
+++ day 28/classwork/cw.py
@@ -0,0 +1,5 @@
+#1)შექმენით სია, რომელიც შეიცავს 6 სხვადასხვა ცხოველის სახელს. წაშალეთ მეორე და მეოთხე ცხოველის სახელი და გამოიტანეთ განახლებული სია.
+cxovelebi = ["lion", "tiger", "elephant", "giraffe", "zebra", "monkey",]
+cxovelebi.pop(1)
+cxovelebi.pop(2)
+print(cxovelebi)
