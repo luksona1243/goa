3e11df426f5d709c9b4bfc75db78974c92720e4a
luksona1243 <luksona1243@gmail.com>
2025-01-25 23:39:09 +0400

    second commit

---
+++ day 28/homework/hw.py
@@ -0,0 +1,4 @@
+#1)შექმენით სია, რომელიც შეიცავს 5 სხვადასხვა ცხოველის სახელს. გამოიტანეთ პირველი და ბოლო ცხოველის სახელი ინდექსების გამოყენებით.
+animalplanet =["jirafi","kenguru","lomi","niangi","ku"]
+print(animalplanet[0])
+print(animalplanet[4])
---
+++ day 28/homework/hw2.py
@@ -0,0 +1,7 @@
+#3)შექმენით სია, რომელიც შეიცავს 6 რიცხვს. გამოიტანეთ 
+# სიის ელემენტების ჯამი და საშუალო მნიშვნელობა.
+num = [1,2,3,4,5]
+damateba =(num[0]+num[1]+num[2]+num[3]+num[4])
+print(damateba)
+sahualo = damateba / len(num)
+print(sahualo)
